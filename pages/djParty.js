b30ff7c82bfbc9ad303828bfe733e9a3cd059417
Leonardo lapDell <leovid2392@gmail.com>
2022-04-21 02:39:58 -0500

    djparty, falta slider de eventos

--- pages/djParty.js
+++ pages/djParty.js
@@ -1,6 +1,26 @@
 import Layout from '../components/layout'
 import useTranslation from 'next-translate/useTranslation'
 
+// <<<<<<<<<components<<<<<<<<<<<<<<<<<<<
+import Hero from '../components/Hero'
+import TitlePage from '../components/TitlePage'
+import Paragraph from '../components/Paragraph'
+import ArticlePortrait from '../components/ArticlePortrait'
+import ArticleFourImg from '../components/ArticleFourImg'
+import ArticleSquare from '../components/ArticleSquare'
+import ArticleSquareWhite from '../components/ArticleSquareWhite'
+import Layout2x2 from '../components/Layout2x2'
+import TitleSection from '../components/TitleSection'
+import Banner from '../components/Banner'
+import Layout1x1 from '../components/Layout1x1'
+//<<<<<<<<< components<<<<<<<<<<<<<<<<<<<
+//<<<<<<<<< images<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+import slide1 from '../public/images/djparty/cele1_small750x500.jpg'
+import slide2 from '../public/images/djparty/mia-5750_small500x750.jpg'
+import slide3 from '../public/images/djparty/cele3_small750x500.jpg'
+
+// <<<<<<<<<images<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 export default function DjParty() {
   let { t } = useTranslation()
   return (
@@ -8,7 +28,36 @@ export default function DjParty() {
       title={t('djParty:metaTitle')}
       description={t('djParty:metaDescription')}
     >
-      <h1>MIA DjParty</h1>
+      <Hero
+        sliderData={[
+          {
+            id: 1,
+            image: slide1,
+          },
+          {
+            id: 2,
+            image: slide2,
+          },
+          {
+            id: 3,
+            image: slide1,
+          },
+        ]}
+      />
+      <TitlePage text='djparty' />
+      <Paragraph text='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel magna ultricies justo fermentum tincidunt.  Cras in sagittis enim. Quisque porta' />
+      <Layout1x1 img={slide3} alt='image description' objectPosition='center' />
+      <TitleSection text='próximos eventos' />
+      <ArticleSquareWhite
+        image={slide2}
+        title='pablo fierro'
+        paragraph='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel magna ultricies justo fermentum tincidunt.  Cras in sagittis enim. Quisque porta'
+        linkText='ir al blog'
+        linkHref='#'
+        alt='image description'
+        objectFit='cover'
+        objectPosition='center'
+      />
     </Layout>
   )
 }
