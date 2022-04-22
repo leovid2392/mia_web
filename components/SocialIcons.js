46f24fe4d03cf7c2f1116d00167c52a44ac65e91
Leonardo lapDell <leovid2392@gmail.com>
2022-04-22 07:31:58 -0500

    hero images added

--- components/SocialIcons.js
+++ components/SocialIcons.js
@@ -17,6 +17,7 @@ export default function SocialIcons({ text }) {
         className={styles.a}
         href='https://www.instagram.com/miatulum/'
         target='_blank'
+        rel='noreferrer'
       >
         <FaInstagram />
       </a>
@@ -24,6 +25,7 @@ export default function SocialIcons({ text }) {
         className={styles.a}
         href='https://www.facebook.com/miatulummx/?ref=page_internal'
         target='_blank'
+        rel='noreferrer'
       >
         <FaFacebookF />
       </a>
@@ -34,6 +36,7 @@ export default function SocialIcons({ text }) {
         className={styles.a}
         href='https://www.google.com/search?sa=X&sxsrf=APq-WBscADPW4XdTOll0uIxGYXIMqOjdsQ:1650599510609&q=mia+tulum+beach+club&ved=2ahUKEwiL5Pur4qb3AhWITTABHVFzCKwQuzF6BAgBEAI&biw=1323&bih=665&dpr=2#:~:text=M%C3%ADa%20Restaurant%20%26%20Beach-,Club,-4.3'
         target='_blank'
+        rel='noreferrer'
       >
         <FaGoogle />
       </a>
@@ -41,12 +44,14 @@ export default function SocialIcons({ text }) {
         className={styles.a}
         href='https://www.tripadvisor.com.mx/Restaurant_Review-g150813-d13722373-Reviews-Mia_Restaurant_Beach_Club-Tulum_Yucatan_Peninsula.html'
         target='_blank'
+        rel='noreferrer'
       >
         <FaTripadvisor />
       </a>
       <a
         href='https://www.tripadvisor.com.mx/Restaurant_Review-g150813-d13722373-Reviews-Mia_Restaurant_Beach_Club-Tulum_Yucatan_Peninsula.html'
         target='_blank'
+        rel='noreferrer'
       >
         <Image
           src={openTableIcon}
