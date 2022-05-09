8e1f800420765c5056b973dec668349f8f07f907
Leonardo lapDell <leovid2392@gmail.com>
2022-05-08 20:09:39 -0500

    google analytics added

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import '../styles/globals.css'
 import { TaskProvider } from '../context/taskContext'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <TaskProvider>
-      <Component {...pageProps} />
-    </TaskProvider>
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-3LB3ZS0S05`}
+      />
+      <Script strategy='lazyOnload'>
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3LB3ZS0S05');
+        `}
+      </Script>
+      <TaskProvider>
+        <Component {...pageProps} />
+      </TaskProvider>
+    </>
   )
 }
 
